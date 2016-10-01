4791424b560e914fa9e645bbf6a2b5fa5123dd8c
Yash Shah <yashshah0127@gmail.com>
2016-10-01 02:16:23 -0700

    add whitenoise

--- mysite/settings.py
+++ mysite/settings.py
@@ -133,3 +133,8 @@ STATICFILES_DIRS = (
 # Update database configuration with $DATABASE_URL.
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
+
+# Simplified static file serving.
+# https://warehouse.python.org/project/whitenoise/
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
